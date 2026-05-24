dbca587833571b3ffcc8dce4be8b532bed22a24e
yashkumar0008 <yashdahiya0008@gmail.com>
2026-05-24 22:28:41 +0530

    update cors

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const errorHandler = require('./middleware/error');
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(fileUpload({ useTempFiles: false, limits: { fileSize: 10 * 1024 * 1024 } }));
