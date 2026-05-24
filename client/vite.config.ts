47463efc33deec8873ceb95bcefe74bb2ba7490a
yashkumar0008 <yashdahiya0008@gmail.com>
2026-05-24 23:03:11 +0530

    update API

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://bda-cmr-backend.vercel.app',
+        target: 'https://bda-cmr-backend.onrender.com',
         changeOrigin: true,
       }
     }
